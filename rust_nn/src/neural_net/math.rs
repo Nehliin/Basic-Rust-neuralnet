bf652e109ddbd9e1dce797157ac9dab053093561
Nehliin <o-nehlin@hotmail.com>
2018-06-19 21:49:41 +0200

    restructured again and added more tests

--- rust_nn/src/neural_net/math.rs
+++ rust_nn/src/neural_net/math.rs
@@ -2,29 +2,35 @@ extern crate rand;
 use std;
 use neural_net::math::rand::distributions::{Normal, Distribution};
 
+#[derive(Debug)]
+pub struct Matrix(pub Vec<Vec<f32>>);
 
 pub fn normally_distributed() -> f32{
     let normal_distribution = Normal::new(0.0, 1.0);
     return normal_distribution.sample(&mut rand::thread_rng()) as f32;
 }
 
-pub fn generate_matrix(number_of_columns: &usize, number_of_rows: &usize) -> Vec<Vec<f32>> {
-    let columns = vec![normally_distributed(); *number_of_rows];
-    return vec![columns; *number_of_columns];
+pub fn generate_matrix(number_of_columns: &usize, number_of_rows: &usize) -> Matrix {
+    let mut matrix = Vec::with_capacity(*number_of_columns);
+    for _ in 0..*number_of_columns {
+        matrix.push(vec![0.0; *number_of_rows].into_iter().map(|_| normally_distributed()).collect()); // generate column
+    }
+
+    return Matrix(matrix);
 }
 
 pub fn sigmoid(x : f32) -> f32{
     return 1.0/(1.0 + std::f32::consts::E.powf(-x));
 }
 
-pub fn matrix_multiply(matrix: &Vec<Vec<f32>>, vec: &Vec<f32>) -> Result<Vec<f32>, &'static str> {
-    if matrix.len() != vec.len() { //row
+pub fn matrix_multiply(matrix: &Matrix, vec: &Vec<f32>) -> Result<Vec<f32>, &'static str> {
+    if matrix.0.len() != vec.len() { //row
         return Err("Missmatch of row lenght and vec to be multiplied")
     } else {
-        let mut result = Vec::with_capacity(matrix.len());
+        let mut result = Vec::with_capacity(matrix.0.len());
         let mut index:usize = 0;
         let mut calc:f32 = 0.0;
-        for i in matrix {
+        for i in &matrix.0 {
             for matrixValue in i {
                 calc += matrixValue*vec[index];
                 index += 1;
@@ -37,4 +43,58 @@ pub fn matrix_multiply(matrix: &Vec<Vec<f32>>, vec: &Vec<f32>) -> Result<Vec<f32
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn matrix_multiplication() {
+        let matrix: Matrix = Matrix(vec![
+            vec![1.0, 1.5, 1.2],
+            vec![2.0, -3.0, 4.5],
+            vec![1.1, 1.2, -1.4]
+        ]);
+        let wrong_size: Vec<f32> = vec![1.2, 2.3, 3.4, 4.0];
+        assert_eq!(true, matrix_multiply(&matrix, &wrong_size).is_err());
+
+        let right_size: Vec<f32> = vec![1.2, 2.3, 3.4];
+        let answer: Vec<f32> = vec![8.73, 10.800001, -0.6800003];
+
+        let calculated = matrix_multiply(&matrix, &right_size);
+
+        let calculated = match calculated {
+            Ok(result) => result,
+            _ => vec![],
+        };
+        assert_eq!(answer, calculated);
+    }
+
+    #[test]
+    fn matrix_generation() {
+        let matrix = generate_matrix(&2,&3);
+        assert_eq!(2, matrix.0.len());
+
+        let mut prev_col =  vec![2.0;3]; // can never be greater than 1
+        for col in &matrix.0 {
+            assert_eq!(3, col.len());
+            assert_ne!(prev_col, *col); // check that columns are unique
+            let mut prev = 2.0 as f32; // a value that can never be taken
+            for x in col {
+                assert_ne!(prev, *x);
+                println!("{}",*x);
+                assert!( -5.0 <= *x && *x <= 5.0);
+                prev = *x;
+            }
+            prev_col = col.clone();
+        }
+    }
+
+    #[test]
+    fn sigmoid_test() {
+        assert_eq!(1.0, sigmoid(345.234));
+        assert_eq!(0.09673856, sigmoid(-2.234));
+        assert_eq!(0.99469614, sigmoid(5.234));
+        assert_eq!(0.77451790 ,sigmoid(1.234))
+    }
+}
 
--- rust_nn/src/neural_net/mod.rs
+++ rust_nn/src/neural_net/mod.rs
@@ -2,10 +2,11 @@
 
 pub mod math;
 
+use neural_net::math::Matrix;
 
 #[derive(Debug)]
-struct NeuralNet {
-    weight_matrixes: Vec<Vec<Vec<f32>>>, // list of matrixes
+pub struct NeuralNet {
+    weight_matrixes: Vec<Matrix>,
     biases: Vec<Vec<f32>>,
     number_of_layers: usize
 }
@@ -14,15 +15,15 @@ impl NeuralNet {
 
     pub fn new(structure: Vec<usize>) -> NeuralNet{
         let mut biases: Vec<Vec<f32>> = Vec::with_capacity(structure.len()-1); // first layer need no bias
-        let mut weight_matrixes: Vec<Vec<Vec<f32>>> = Vec::with_capacity(structure.len()-1); // last layer have no matrix to feed forward
+        let mut weight_matrixes: Vec<Matrix> = Vec::with_capacity(structure.len()-1); // last layer have no matrix to feed forward
         let number_of_layers = structure.len();
 
         for i in 1..number_of_layers {
             let mut bias_layer: Vec<f32> = vec![0.0; structure[i]]; // allocate each bias vector for each layer
             biases.push(bias_layer.into_iter().map(|_| math::normally_distributed()).collect()); // fill with normally distributed values
 
-            if let Some(nr_neurons) = structure.get(i+1) {
-                weight_matrixes.push(math::generate_matrix(&structure[i], nr_neurons));
+            if let Some(nr_neurons) = structure.get(i) {
+                weight_matrixes.push(math::generate_matrix(&structure[i-1], nr_neurons));
             }
 
         }
@@ -42,5 +43,44 @@ impl NeuralNet {
         //    result.push(math::sigmoid(missing_bias[index] + &self.bias[index]));
         //}
         return Err("penis");
+    }
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn nn_new() {
+        let structure: Vec<usize> = vec![2,3, 5];
+
+        let nn = NeuralNet::new(structure);
+
+        assert_eq!(3, nn.number_of_layers);
+        assert_eq!(2, nn.biases.len());
+        assert_eq!(2, nn.weight_matrixes.len());
+
+        assert_ne!(nn.biases[0], nn.biases[1]);
+
+        assert_eq!(2, nn.weight_matrixes[0].0.len()); //number of columns in connecting matrix between layer 0 and 1
+        let row_count = 3;
+        let mut prev_col = vec![0.0, 0.0, 0.0];
+        for col in &nn.weight_matrixes[0].0 {
+            assert_eq!(row_count, col.len());
+            assert_ne!(prev_col, *col);
+            prev_col = col.clone();
+        }
+
+
+        assert_eq!(3, nn.weight_matrixes[1].0.len()); //number of columns in connecting matrix between layer 0 and 1
+        let row_count = 5;
+        let mut prev_col = vec![0.0;5];
+        for col in &nn.weight_matrixes[1].0 {
+            assert_eq!(row_count, col.len());
+            assert_ne!(prev_col, *col);
+            prev_col = col.clone();
+        }
     }
 }
