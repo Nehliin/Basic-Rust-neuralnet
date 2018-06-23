a198e58a557fee75a4070f5bee141a973d43b7fd
Oskar Nehlin <o-nehlin@hotmail.com>
2018-06-23 14:20:56 +0200

    implemented image loader

--- rust_nn/src/image_loader.rs
+++ rust_nn/src/image_loader.rs
@@ -1,50 +1,45 @@
+extern crate image;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
 use std::io::{self, BufReader};
 
-struct Image(Vec<Vec<u8>>);
+const number_of_images:usize = 60000;
+const number_of_rows:usize = 28;
+const number_of_columns:usize = 28;
+
+struct Image(Vec<u8>);
 
 impl Image {
-    fn draw(&self) {
-        for row in &self.0 {
-            println!("{:?}",row);
+    fn create(&self) {
+        let mut image_buffer = image::GrayImage::new(number_of_columns as u32, number_of_rows as u32);
+        for (x, y, pixel) in image_buffer.enumerate_pixels_mut() {
+            let index:usize = (y as usize)*number_of_rows + x as usize;
+            *pixel = image::Luma([self.0[index]]);
         }
+        File::create("img.png").unwrap();
+        image_buffer.save("img.png");
     }
+
 }
 
 fn load() -> Result<()> {
     let mut image_file = File::open("train-images-idx3-ubyte")?;
-    const number_of_images:usize = 60000;
-    const number_of_rows:usize = 28;
-    const number_of_columns:usize = 28;
     let mut buf = BufReader::with_capacity(number_of_images*number_of_columns*number_of_rows, image_file);
-  //  let mut buf =   [0;number_of_images*number_of_columns*number_of_rows];
-  //  image_file.read(buf.as_mut())?;
     if let Ok(bytes) = buf.fill_buf() {
-        print!("hej");
+        println!("Start");
         let mut images:Vec<Image> = Vec::with_capacity(number_of_images);
-        let mut rows = Vec::with_capacity(number_of_rows);
-        let mut row = Vec::with_capacity(number_of_columns);
-        print!("start");
-        for (i, pixel) in bytes[16..].iter().enumerate() { // first 16 bytes are meta-data
-            if i % number_of_rows*number_of_columns == 0 {
-                println!("första {}", i % number_of_rows*number_of_columns);
-                images.push(Image(rows.clone()));
-                rows.clear();
-                println!("images len, {}", images.len());
-                if images.len() == 3 {
-                    break;
-                }
+        let mut pixels:Vec<u8> = Vec::with_capacity(number_of_columns*number_of_rows);
+        for (i, pixel) in bytes[16..].iter().enumerate() { // first 16 bytes are metadata
+            if i % (number_of_rows*number_of_columns) == 0 {
+                images.push(Image(pixels.clone()));
+                pixels.clear();
             }
-            row.push(*pixel);
-            if i % number_of_rows == 0 {
-                println!("andra {}", i % number_of_rows);
-                rows.push(row.clone());
-                row.clear();
-            }
+            pixels.push(*pixel);
         }
-        images[2].draw();
+        println!("Done");
+        images[6].create();
     }
 
     Ok(())
